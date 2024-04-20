d2071e0fa77066d0dd9c1ddc7259e65b15e16a05
nathen321 <walidjalaly43@gmail.com>
2024-04-19 17:58:04 -0700

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,5 @@
+# Install an especific version of flask
+
 exec { 'flask':
   command => '/usr/bin/pip3 install Flask'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,5 @@
+#kill it with fire
+
 exec { 'killmenow':
   command => '/usr/bin/pkill killmenow'
 }
