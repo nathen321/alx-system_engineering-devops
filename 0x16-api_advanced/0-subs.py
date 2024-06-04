d1fcd664a5d8ef3e4ef8333c5d162cf08c7bd4b8
nathen321 <walidjalaly43@gmail.com>
2024-06-04 16:00:36 -0700

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ def number_of_subscribers(subreddit):
     import requests
 
     sub = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "linux:/ludaw/0.1"},
-                            allow_redirects=False)
+                       .format(subreddit),
+                       headers={"User-Agent": "linux:/ludaw/0.1"},
+                       allow_redirects=False)
     if sub.status_code >= 300:
         return 0
 
