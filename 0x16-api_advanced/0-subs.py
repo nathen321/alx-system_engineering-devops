e530d4fb85a22add59bc9f24ff68f1a2aad25c7b
nathen321 <walidjalaly43@gmail.com>
2024-06-04 15:23:42 -0700

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Module for task 0"""
-
 import json
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Read reddit API and return number subscribers """
@@ -10,12 +10,12 @@ def number_of_subscribers(subreddit):
     sk = 'MB5wiAqzpkow7aZsKsK1lQvlo6W3SQ'
     auth = requests.auth.HTTPBasicAuth(cl_id, sk)
     data = {'grant_type': 'password',
-        'username': 'TrainingFly4822',
-        'password': 'C@l1gula'}
+            'username': 'TrainingFly4822',
+            'password': 'C@l1gula'}
 
     headers = {'User-Agent': 'MyBot/1.0.1'}
     res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, data=data, headers=headers)
+                        auth=auth, data=data, headers=headers)
     TOKEN = res.json()['access_token']
     headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
     sub_info = requests.get("https://oauth.reddit.com/r/{}/about.json"
