5b681fdeb0a9a1462630c0920584e765bd132cc4
nathen321 <walidjalaly43@gmail.com>
2024-06-04 15:50:24 -0700

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
 """Module for task 0"""
-import requests
 
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers """
-    cl_id = '3WrsYgFIARz1D-hUyTDBMg'
-    sk = 'MB5wiAqzpkow7aZsKsK1lQvlo6W3SQ'
-    auth = requests.auth.HTTPBasicAuth(cl_id, sk)
-    data = {'grant_type': 'password',
-            'username': 'TrainingFly4822',
-            'password': 'C@l1gula'}
+    """Queries the Reddit API"""
+    import requests
 
-    headers = {'User-Agent': 'MyBot/1.0.1'}
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-                        auth=auth, data=data, headers=headers)
-    TOKEN = res.json()['access_token']
-    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
-    sub_info = requests.get("https://oauth.reddit.com/r/{}/about.json"
-                            .format(subreddit), headers=headers,
+    sub = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "linux:/ludaw/0.1"},
                             allow_redirects=False)
-    print(sub_info.status_code)
-    if sub_info.status_code >= 300:
+    if sub.status_code >= 300:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    return sub.json().get("data").get("subscribers")
