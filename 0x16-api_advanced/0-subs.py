88b9a8783bc0d9a9f513ab4a41c7e60d3808b2e5
walid jalaly <46631425+nathen321@users.noreply.github.com>
2024-07-18 16:49:09 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """Module for task 0"""
-import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API"""
-    sub = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subreddit),
-                       headers={"User-Agent": "linux:/ludaw/0.1"},
-                       allow_redirects=False)
-    if sub.status_code >= 300:
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-    return sub.json().get("data").get("subscribers")
+    return sub_info.json().get("data").get("subscribers")
